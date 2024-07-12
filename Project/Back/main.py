2641902e5d8345e5f081fa6fa341aa33ca7743ea
Christian-Baez-Dev <christianbaezdev@gmail.com>
2024-07-12 10:54:57 -0400

    cambio de nombre del controller

--- Project/Back/main.py
+++ Project/Back/main.py
@@ -3,10 +3,9 @@ from Project.Back.request import request_user
 from Project.Back.sevice import servicio
 app = FastAPI()
 
-@app.post("/prueba/")
-def prueba(user: request_user):
+@app.post("/predictor/")
+def predictor(user: request_user):
     try:
-        print('entro')
         return servicio.generarResponse(user)
     except Exception as e:
         return e
